10df8876ee9da27713828d290a572420b31ee5e7
aayush <hiaayush30@gmail.com>
2025-08-28 18:28:54 +0530

    commit

--- client/src/app/(authenticated)/dashboard/AuthProvider.tsx
+++ client/src/app/(authenticated)/dashboard/AuthProvider.tsx
@@ -2,15 +2,27 @@
 import { useEffect } from "react";
 import { useAuthStore } from "@/store/useAuthStore";
 import { useRouter } from "next/navigation";
+import jwt from "jsonwebtoken";
+import { IUserToken } from "./layout";
+import { Loader } from "lucide-react";
 
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
-  const { user } = useAuthStore();
+  const { user, setUser } = useAuthStore();
   const router = useRouter();
   useEffect(() => {
     if (!user) {
-      router.replace("/login")
+      const token = localStorage.getItem("token");
+      if (!token) {
+        return router.replace("/login")
+      }
+      const user = jwt.decode(token) as IUserToken;
+      setUser(user)
     }
-  }, [router, user]);
+  }, [router, setUser, user]);
+
+  if (!user) {
+    return <div className="min-h-screen flex items-center justify-center"><Loader className="animate-spin" /></div>
+  }
 
   return <>{children}</>;
 }
